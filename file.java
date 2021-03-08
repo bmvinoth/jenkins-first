bfd2ed3aeafaaa8e5490b16d4449c8bc7217c44e
bmvinoth <65478616+bmvinoth@users.noreply.github.com>
2021-03-08 20:45:47 +0530

    Update file.java

--- file.java
+++ file.java
@@ -1,5 +1,5 @@
-public class hello {
-public static void main(string[] args) {
-system.out.println("Hello world");
-}
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+  }
 }
